2036b088887f354aea14312c7a8d5fbe5e7cd324
Cristian Camargo <chris.camargo2015@gmail.com>
2021-08-24 20:05:22 -0400

    criando evento de click, e mostrando em qual celula foi clicado

--- src/app/tic-tac-toe/tic-tac-toe.component.ts
+++ src/app/tic-tac-toe/tic-tac-toe.component.ts
@@ -8,11 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class TicTacToeComponent{
 
   currentPlayer: string = "O";
+  winner:string = "";
+
   board: string[][] = [
     ['','',''],
     ['','',''],
     ['','','']
   ];
+
+
+
+  processPlay(line: number, col:number){
+
+    console.log(`Jogada na linha ${line}, coluna ${col} do jogador ${this.currentPlayer}`)
+
+  }
 
   constructor() { }
 
